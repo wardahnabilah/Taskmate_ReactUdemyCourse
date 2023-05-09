22207772bfc55fedfa12f3eaaba97ff974745dcf
unknown <wardahnabilah33@gmail.com>
2023-05-09 21:09:14 +0700

    Add AddTask component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import AddTask from './components/AddTask';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AddTask />
     </div>
   );
 }
---
+++ src/components/AddTask.js
@@ -0,0 +1,10 @@
+export default function AddTask() {
+    return (
+        <div className="addTask"> 
+            <form action="">
+                <input type="text" placeholder="add task" />
+                <button>Add</button>
+            </form>
+        </div>
+    )
+}
