823ec253f7a2e6d883e73d67580e01ddb84a3b43
unknown <wardahnabilah33@gmail.com>
2023-05-09 21:46:11 +0700

    Add ShowTask component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import AddTask from './components/AddTask';
 import Header from './components/Header';
+import ShowTask from './components/ShowTask';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AddTask />
+      <ShowTask />
     </div>
   );
 }
---
+++ src/components/ShowTask.js
@@ -0,0 +1,13 @@
+export default function ShowTask() {
+    return (
+        <section className="showTask">
+            <p className="head">
+                <span>
+                    <span className="title">Todo</span>
+                    <span className="count">2</span>
+                </span>
+                <span className="clearAll">Clear All</span>
+            </p>
+        </section>
+    )
+}
