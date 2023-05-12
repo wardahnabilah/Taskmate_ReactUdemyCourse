1e94337d862957e014327a43120c73465fa99f91
unknown <wardahnabilah33@gmail.com>
2023-05-12 14:37:58 +0700

    Fix 'Add New Task' feature

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -25,13 +25,18 @@ export default function AddTask(props) {
 
     function submitTask(event) {
         props.addTask(newTask)
+        setNewTask({
+            id: "",
+            task: "",
+            time: ""
+        })
         event.preventDefault()
     }
     
     return (
         <div className="addTask"> 
             <form onSubmit={submitTask}>
-                <input onChange={handleChange} type="text" placeholder="add task" />
+                <input onChange={handleChange} value={newTask.task} type="text" placeholder="add task" />
                 <button>Add</button>
             </form>
         </div>
