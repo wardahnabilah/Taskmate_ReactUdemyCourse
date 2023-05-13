26f3b4485be5a4e7f1197a7aaed0ed2b42fac862
unknown <wardahnabilah33@gmail.com>
2023-05-13 23:27:59 +0700

    Add 'Change Theme' feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,34 @@
+import { useState } from 'react'
 import logo from '../assets/logo.svg'
 
 export default function Header() {
+    const [theme, setTheme] = useState("light")
+    
+    // Change theme
+    function changeTheme(event) {
+        const chosenTheme = event.target.dataset.theme
+        setTheme(chosenTheme)
+
+        const htmlElement = document.documentElement
+        // Remove previous class for theme
+        htmlElement.removeAttribute("class")
+        // Add new theme class
+        htmlElement.classList.add(chosenTheme)
+    }
+
     return (
-        <header class="header">
+        <header className="header">
             <div className="logo">
                 <img src={logo} alt="" />
                 <span>Taskmate</span>
             </div>
             <div className="themeSelector">
-                <span className="light activeTheme"></span>
-                <span className="medium"></span>
-                <span className="dark"></span>
-                <span className="gradientOne"></span>
-                <span className="gradientTwo"></span>
-                <span className="gradientThree"></span>
+                <span onClick={changeTheme} data-theme="light" className={theme === "light" ? "light activeTheme" : "light"}></span>
+                <span onClick={changeTheme} data-theme="medium" className={theme === "medium" ? "medium activeTheme" : "medium"}></span>
+                <span onClick={changeTheme} data-theme="dark" className={theme === "dark" ? "dark activeTheme" : "dark"}></span>
+                <span onClick={changeTheme} data-theme="gradientOne" className={theme === "gradientOne"? "gradientOne activeTheme" : "gradientOne"}></span>
+                <span onClick={changeTheme} data-theme="gradientTwo" className={theme === "gradientTwo" ? "gradientTwo activeTheme" : "gradientTwo"}></span>
+                <span onClick={changeTheme} data-theme="gradientThree" className={theme === "gradientThree" ? "gradientThree activeTheme" : "gradientThree"}></span>
             </div>
         </header>
     )
